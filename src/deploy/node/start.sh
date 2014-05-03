1a8674202c269587254ffeaba5d9727f749b7738
David Souther <davidsouther@gmail.com>
2014-05-02 21:05:45 -0400

    Cleaned up node start.

--- src/deploy/node/start.sh
+++ src/deploy/node/start.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-ROOTDIR="$(cd "$(dirname $0)" && pwd | sed 's!/src/deploy!!')"
+ROOTDIR="$(cd "$(dirname $0)" && pwd | sed 's!/src/deploy/node!!')"
 echo "Starting in $ROOTDIR"
 
 source $ROOTDIR/etc/environment.sh
 
 [ -f $ROOTDIR/var/mongo.pid ] || $ROOTDIR/src/deploy/mongo/start.sh
 
+sleep 1 # Let mongod start
+
 node   $ROOTDIR/node_modules/.bin/supervisor \
     -w $ROOTDIR/src/server -e coffee \
-       $ROOTDIR/src/deploy/start.js
+       $ROOTDIR/src/deploy/node/start.js
